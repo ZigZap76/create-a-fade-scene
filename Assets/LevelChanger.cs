f1f1f58c5fad38fc4f275ae77ba8af26ee190d6f
ZigZap76 <oussema.gargouri76@gmail.com>
2018-08-06 14:39:30 +0100

    gg

---
+++ Assets/LevelChanger.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+    public int scenetoload;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            FadeToLevel(1);
+        StartCoroutine("invoke");
+	}
+
+
+
+    public void FadeToLevel (int index)
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+
+    public void OnFadeComplete ()
+    {
+        SceneManager.LoadScene(scenetoload);
+    }
+
+    IEnumerator invoke()
+    {
+        yield return new WaitForSeconds(3);
+        Debug.Log("gg");
+        OnFadeComplete();
+        
+        
+
+    }
+    
+}
